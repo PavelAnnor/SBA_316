37665ed6b31b548e540a7d63b7d66192f1e85fd0
PavelAnnor <annorpavel@gmail.com>
2026-05-28 19:06:42 -0400

    Started curtain slide up animation

---
+++ characters.js
@@ -0,0 +1,449 @@
+export const characters = [
+
+  {
+    name: "Alien",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: true, pack: "Kombat Pack 2" },
+    variations: [
+      { variationName: "Acidic", variationDescription: "" },
+      { variationName: "Konjurer", variationDescription: "" },
+      { variationName: "Tarkatan", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Cassie Cage",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Hollywood", variationDescription: "" },
+      { variationName: "Brawler", variationDescription: "" },
+      { variationName: "Spec Ops", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "D'Vorah",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Brood Mother", variationDescription: "" },
+      { variationName: "Swarm Queen", variationDescription: "" },
+      { variationName: "Venomous", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Ermac",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Mystic", variationDescription: "" },
+      { variationName: "Spectral", variationDescription: "" },
+      { variationName: "Master of Souls", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Erron Black",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Marksman", variationDescription: "" },
+      { variationName: "Gunslinger", variationDescription: "" },
+      { variationName: "Outlaw", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Ferra & Torr",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Ruthless", variationDescription: "" },
+      { variationName: "Vicious", variationDescription: "" },
+      { variationName: "Lackey", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Goro",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: true, pack: "Kombat Pack 1" },
+    variations: [
+      { variationName: "Dragon Fangs", variationDescription: "" },
+      { variationName: "Kuatan Warrior", variationDescription: "" },
+      { variationName: "Tigrar Fury", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Jacqui Briggs",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "High Tech", variationDescription: "" },
+      { variationName: "Shotgun", variationDescription: "" },
+      { variationName: "Full Auto", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Jax",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Pumped Up", variationDescription: "" },
+      { variationName: "Heavy Weapons", variationDescription: "" },
+      { variationName: "Wrestler", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Johnny Cage",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "A-List", variationDescription: "" },
+      { variationName: "Fistcuffs", variationDescription: "" },
+      { variationName: "Stunt Double", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Kano",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Cybernetic", variationDescription: "" },
+      { variationName: "Commando", variationDescription: "" },
+      { variationName: "Cutthroat", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Kenshi",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Kenjutsu", variationDescription: "" },
+      { variationName: "Balanced", variationDescription: "" },
+      { variationName: "Possessed", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Kitana",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Mournful", variationDescription: "" },
+      { variationName: "Royal Storm", variationDescription: "" },
+      { variationName: "Assassin", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Kotal Kahn",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Blood God", variationDescription: "" },
+      { variationName: "War God", variationDescription: "" },
+      { variationName: "Sun God", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Kung Jin",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Shaolin", variationDescription: "" },
+      { variationName: "Ancestral", variationDescription: "" },
+      { variationName: "Bojutsu", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Kung Lao",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Buzz Saw", variationDescription: "" },
+      { variationName: "Tempest", variationDescription: "" },
+      { variationName: "Hat Trick", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Liu Kang",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Dragon's Fire", variationDescription: "" },
+      { variationName: "Flame Fist", variationDescription: "" },
+      { variationName: "Dualist", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Mileena",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Ethereal", variationDescription: "" },
+      { variationName: "Piercing", variationDescription: "" },
+      { variationName: "Ravenous", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Predator",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: true, pack: "Kombat Pack 1" },
+    variations: [
+      { variationName: "Hunter", variationDescription: "" },
+      { variationName: "Warrior", variationDescription: "" },
+      { variationName: "Hish-Qu-Ten", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Quan Chi",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Warlock", variationDescription: "" },
+      { variationName: "Summoner", variationDescription: "" },
+      { variationName: "Sorcerer", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Raiden",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Thunder God", variationDescription: "" },
+      { variationName: "Displacer", variationDescription: "" },
+      { variationName: "Master of Storms", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Reptile",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Noxious", variationDescription: "" },
+      { variationName: "Deceptive", variationDescription: "" },
+      { variationName: "Nimble", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Scorpion",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Ninjutsu", variationDescription: "" },
+      { variationName: "Hellfire", variationDescription: "" },
+      { variationName: "Inferno", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Shinnok",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Necromancer", variationDescription: "" },
+      { variationName: "Imposter", variationDescription: "" },
+      { variationName: "Bone Shaper", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Sonya Blade",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Special Forces", variationDescription: "" },
+      { variationName: "Covert Ops", variationDescription: "" },
+      { variationName: "Demolition", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Sub-Zero",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Cryomancer", variationDescription: "" },
+      { variationName: "Unbreakable", variationDescription: "" },
+      { variationName: "Grandmaster", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Takeda",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: false, pack: null },
+    variations: [
+      { variationName: "Ronin", variationDescription: "" },
+      { variationName: "Shirai Ryu", variationDescription: "" },
+      { variationName: "Lasher", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Tanya",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: true, pack: "Kombat Pack 1" },
+    variations: [
+      { variationName: "Kobu Jutsu", variationDescription: "" },
+      { variationName: "Dragon Naginata", variationDescription: "" },
+      { variationName: "Pyromancer", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Triborg",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: true, pack: "Kombat Pack 2" },
+    variations: [
+      { variationName: "Sektor", variationDescription: "" },
+      { variationName: "Cyrax", variationDescription: "" },
+      { variationName: "Smoke", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Jason Voorhees",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: true, pack: "Kombat Pack 1" },
+    variations: [
+      { variationName: "Slasher", variationDescription: "" },
+      { variationName: "Unstoppable", variationDescription: "" },
+      { variationName: "Relentless", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Leatherface",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: true, pack: "Kombat Pack 2" },
+    variations: [
+      { variationName: "Butcher", variationDescription: "" },
+      { variationName: "Pretty Lady", variationDescription: "" },
+      { variationName: "Killer", variationDescription: "" }
+    ]
+  },
+
+  {
+    name: "Bo' Rai Cho",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: true, pack: "Kombat Pack 2" },
+    variations: [
+      { variationName: "Bartitsu", variationDescription: "" },
+      { variationName: "Dragon Breath", variationDescription: "" },
+      { variationName: "Drunken Master", variationDescription: "" }
+    ]
+  }
+
+];
+
+
+/*{
+    name: "Alien",
+    img: "",
+    bio: "",
+    quote: "",
+    dlc: { fromDlc: true, pack: "Kombat Pack 2" },
+    variations: [
+      { variationName: "Acidic", variationDescription: "" },
+      { variationName: "Konjurer", variationDescription: "" },
+      { variationName: "Tarkatan", variationDescription: "" }
+    ]
+  },
+*/
+
+function getCharacter(name){
+
+  return characters.find( (element)=> element.name.toLowerCase() === name.toLowerCase())
+
+}
+
+
+
+
+
+
+
+
+
