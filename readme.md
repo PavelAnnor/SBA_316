4604ee7af57641a7d94812c8111c75edde0d6c55
PavelAnnor <annorpavel@gmail.com>
2026-05-31 21:00:56 -0400

    finalized project

---
+++ readme.md
@@ -0,0 +1,25 @@
+SBA 316: The Document Object Model
+Date: 05/31/26
+
+## 📋 Description
+Project created for Per Scholas Software Engineering Track intended to show profiency with D.O.M. concepts.
+
+A simple website that displays information about charcaters from the video Game Mortal Kombat X. Charcaters in the game 
+have different variations that change their abilities and playstyles. This websites displays information such as name, character bio, 
+images etc.  
+
+## 🔧 Technology
+HTML, CSS, JS
+
+## 💡 Notes
+N/A
+
+
+## 🔗 References
+Images taken from: https://www.fightersgeneration.com/games/mkx-p4.html, https://www.mortalkombatwarehouse.com/
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -3,9 +3,6 @@ import {characters} from "./characters.js"
 import { getCharacter } from "./characters.js";
 
 
-
-
-
 //MAJOR SECTIONS
 
 //Overlay that initially covers entire screen 
@@ -23,6 +20,7 @@ const mainDisplay = document.getElementById("mainDisplay")
 //all the images in the curtain
 const curtainImgs = curtain.getElementsByTagName("img");
 
+//Counter Variables
 let nxt = 1;
 
 const loadingScreenLoop2 = setInterval(()=>{
@@ -42,99 +40,6 @@ const imgSrcs = ['./src/loadingscreens/loadingScreen1.jpeg',
   "./src/loadingscreens/loadingScreen5.jpeg"
 
 ]
-
-function loopThroughImgs2(nxt) {
-    // console.log("starting")
-
-    //img u want to fade in
-    let fadeInImg = document.getElementById("fadeIn");
-    
-    //img u want to fadeout
-    let fadeOutImg = document.getElementById("fadeOut");
-
-    //fade in the image
-    fadeInImg.classList.toggle("fadeInAnimation");
-    fadeOutImg.classList.toggle("fadeOutAnimation")
-    
-    //wait till animation is done
-    setTimeout(()=>{
-
-      //save the src of the fadeout img
-        fadeOutImg.src = fadeInImg.src //switch the img srcs
-        fadeInImg.src = imgSrcs[nxt]
-
-        
-
-        //return back to normal so u can fade again
-        fadeOutImg.classList.remove("fadeOutAnimation")
-        fadeInImg.classList.remove("fadeInAnimation")
-    },5000)
-    
-}
-
-//for loop to create the required amount of cards depending on how many object with imgs in our characters array 
-for(let character of characters){
-   
-  
-      
-    //dynamically create rows based on cardcount so everyhting fits 
-    if (character.img != "") {
-     
-
-    
-      let card = document.createElement("div");//create the card
-      card.classList.add("card");
-      card.id = character.name
-
-      let text = document.createElement("h1");// some text
-      text.textContent = character.name;
-      text.classList.add("cardNameText");
-
-      let img = document.createElement("img"); //the img
-      img.src = character.img;
-      img.classList.add("cardImg")
-     
-      let variationsDiv ;//the variatiosn div
-      
-      card.appendChild(img)
-      card.appendChild(text);
-      //apend variation dive last 
-     
-      //create a fragment 
-      let frag = document.createDocumentFragment();
-
-      //use that frag to appened variations div
-      createVariations(frag)
-      card.appendChild(frag)
-
-      let vari  =Array.from(card.getElementsByClassName("variationThird"))
-      for(let v in vari){
-        let text = document.createElement("p")
-        text.textContent = character.variations[v].variationName
-        text.classList.add("variationText")
-        vari[v].appendChild(text)
-
-      }
-
-      
-      
-
-      //add the event listeners 
-      card.addEventListener("mouseover", variationsAppear);
-      card.addEventListener("mouseout",variationsDisappear)
-      
-
-      characterGrid.appendChild(card);
-      
-    }
-
-    
-
-   
-   
-
-}
-
 
 
 function createVariations(frag){
@@ -174,9 +79,6 @@ function variationsDisappear(event){
 
 
 
-
-curtain.addEventListener("click",liftCurtain)
-
 //When the user clicks, moves the curtain up, unveil the rest of the page, delete the curtain element 
 function liftCurtain(){
     // console.log("Lifting curtain")
@@ -198,7 +100,7 @@ function displayVariationInfo(event){
   let variationName = event.currentTarget.children[0].textContent
 
   //get the corresponding charcater object from the array  
-  let charName = event.currentTarget.parentElement.parentElement.id
+  let charName = event.currentTarget.parentNode.parentElement.id
   let x = getCharacter(charName)
 
   
@@ -220,13 +122,18 @@ function displayVariationInfo(event){
 
   //if its a new card remove active from all other variations and cards
   if(!currentCard.classList.contains("active-card")){
+    
     let allvariations = document.getElementsByClassName("variationThird")
     for(let x of allvariations) 
       x.classList.remove("active-variation")
 
     let allCards = document.getElementsByClassName("card")
-     for(let x of allCards) 
+     for(let x of allCards){
       x.classList.remove("active-card")
+      x.addEventListener("mouseout",variationsDisappear)
+
+     }
+     currentCard.removeEventListener("mouseout",variationsDisappear)
 
       //populate the main display and open it 
       mainDisplay.children[0].textContent = x.bio
@@ -235,12 +142,15 @@ function displayVariationInfo(event){
       mainDisplay.style.backgroundImage = `url(${x.wallpaper})` 
       mainDisplay.style.display = "block"
       
+
+      
       //add active classes
       currentCard.classList.add("active-card")
       currentVari.classList.add("active-variation")
-
+      //scroll them to the main display
+      window.scrollTo({
+        top: 300});
       return
-
   }
 
 
@@ -260,6 +170,7 @@ function displayVariationInfo(event){
 
     //add active to current variation 
     currentVari.classList.add("active-variation")
+    window.scrollTo({top: 300 });
     return
     }
 
@@ -268,64 +179,112 @@ function displayVariationInfo(event){
       mainDisplay.style.display = "none";
       currentVari.classList.remove("active-variation")
       currentCard.classList.remove("active-card")
+      currentCard.addEventListener("mouseout",variationsDisappear)
       return
     }
       
 
+  }
+
+}
+
+
+   function loopThroughImgs2(nxt) {
+    // console.log("starting")
+
+    //img u want to fade in
+    let fadeInImg = document.getElementById("fadeIn");
+    
+    //img u want to fadeout
+    let fadeOutImg = document.getElementById("fadeOut");
+
+    //fade in the image
+    fadeInImg.classList.toggle("fadeInAnimation");
+    fadeOutImg.classList.toggle("fadeOutAnimation")
+    
+    //wait till animation is done
+    setTimeout(()=>{
+
+      //save the src of the fadeout img
+        fadeOutImg.src = fadeInImg.src //switch the img srcs
+        fadeInImg.src = imgSrcs[nxt]
+
+        
+
+        //return back to normal so u can fade again
+        fadeOutImg.classList.remove("fadeOutAnimation")
+        fadeInImg.classList.remove("fadeInAnimation")
+    },5000)
+    
+}
 
 
 
+
+  curtain.addEventListener("click", liftCurtain);
+  for (let character of characters) {
+    //dynamically create rows based on cardcount so everyhting fits
+    if (character.img != "") {
+      let card = document.createElement("div"); //create the card
+      card.classList.add("card");
+      card.id = character.name;
+
+      let text = document.createElement("h1"); // some text
+      text.textContent = character.name;
+      text.classList.add("cardNameText");
+
+      let img = document.createElement("img"); //the img
+      img.src = character.img;
+      img.classList.add("cardImg");
+
+      let variationsDiv; //the variatiosn div
+
+      card.appendChild(img);
+      card.appendChild(text);
+      //apend variation dive last
+
+      //create a fragment
+      let frag = document.createDocumentFragment();
+
+      //use that frag to appened variations div
+      createVariations(frag);
+      card.appendChild(frag);
+
+      let vari = Array.from(card.getElementsByClassName("variationThird"));
+      for (let v in vari) {
+        let text = document.createElement("p");
+        text.textContent = character.variations[v].variationName;
+        text.classList.add("variationText");
+        vari[v].appendChild(text);
+      }
+
+      //add the event listeners
+      card.addEventListener("mouseover", variationsAppear);
+      card.addEventListener("mouseout", variationsDisappear);
+
+      characterGrid.appendChild(card);
+    }
   }
 
 
-  //   //if you click on an active vari, close the main display remove active from variation and card
-  // if(currentVari.classList.contains("active-variation")){
-  //   mainDisplay.style.display = "none"
-  //   currentVari.classList.remove("active-variation")
-  //   currentCard.classList.remove("active-card")
-  //   return
-  //  }
-   
-  //  //if its a new variation
-  //  if(!currentVari.classList.contains("active-variation")){
-  //   currentVari.classList.add("active-variation")
-  //   mainDisplay.children[0].textContent = x.bio
-  //   mainDisplay.children[1].textContent = variationName
-  //   mainDisplay.children[2].textContent = x.variations.find((element)=>element.variationName==variationName).variationDescription
-    
-
-  //   //if its a new card
-  //   if(!currentCard.classList.contains("active-card")){
-  //     mainDisplay.style.display = "block"
-  //     currentCard.classList.add("active-card")
-  //     mainDisplay.style.backgroundImage = `url(${x.wallpaper})`
-  //     mainDisplay.style.display = "block"
-
-  //   }
-    
-
-
-
-  //  }
-
-
-
-  //  //assign active to the card
-  //  currentCard.classList.add("active")
-
-  //  //rmoeve active from other variations 
-  //  for(x of variations)
-  //   x.classList.remove("active")
-
-  
 
 
 
 
-  //   //toggle active on current variation 
-  //  currentVari.classList.add("active")
 
-   }
+
+
+
+
+
+//for loop to create the required amount of cards depending on how many object with imgs in our characters array 
+
+
+
+
+
+
+
 
    
 
@@ -337,28 +296,6 @@ function displayVariationInfo(event){
   
   
 
-  // event.target.
-
-  // console.log(mainDisplay.style.display==""|| mainDisplay.style.display == "none");
-  // //toggle the class of active on the card. 
-  // event.currentTarget.parentElement.parentElement.classList.toggle("active")
-
-  // //if the card is already active, dont close the main display, just change the text (works if u select a variation on the same character card)
-  // if(!event.currentTarget.parentElement.parentElement.classList.contains("active")){
-
-  //   //set the text conent and required backgroudn image
-  
-  //   mainDisplay.style.backgroundImage = `url(${x.wallpaper})`
- 
-
-  // }
-
-  // else{
-  //   mainDisplay.style.display = "block"
-
-  // }
-
- 
  
 
   
